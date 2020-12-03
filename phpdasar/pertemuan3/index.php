3f9901fefac48f8571f9daf5a1699fa4841310c7
Fathul Muiin <fmuiin14@gmail.com>
2020-12-03 22:45:21 +0700

    5 struktur kendali

---
+++ phpdasar/pertemuan3/index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Perulangan
+for, while, do while, foreach (untuk array)
+*/
+
+// for($i=0; $i<5; $i++) {
+//     echo "Hello World";
+//     echo "<br>";
+// }
+// $a = 0;
+// while ($a <= 10) {
+//     echo "Hello World <br>";
+//     $a++;
+// }
+
+// $a = 20;
+
+// do {
+//     echo "Hello World <br>";
+//     $a++;
+// } while ($a <= 10);
+
+?>
